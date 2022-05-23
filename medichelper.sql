61a9dbf5f6d9fdf4f6d28986b414895896a4b790
SamiulMahn034 <mmahin181034@bscse.uiu.ac.bd>
2022-05-23 08:48:08 +0600

    database

--- medichelper.sql
+++ medichelper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2022 at 05:51 PM
+-- Generation Time: May 23, 2022 at 04:47 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.15
 
@@ -46,8 +46,32 @@ INSERT INTO `admin` (`id`, `employee_name`, `contact_number`, `address`, `job_ti
 (2, 'Naba', '01686963558', 'Dhaka', 'Admin', 'mhfz', '1234'),
 (3, 'Shuvo', '01686963558', 'Dhanmondi', 'Manager', 'Shv', '1234'),
 (4, 'Dihan', '01304555701', 'Dhaka', 'Senior Executive', 'Dhn', '1234'),
-(5, 'Shakawat', '01304555701', 'Natunbazar', 'Senior  Executive', 'Shk', '1234'),
-(6, '', '01', 'aa', '', 'asc', '1234');
+(5, 'Shakawat', '01304555701', 'Natunbazar', 'Senior  Executive', 'Shk', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appoinment`
+--
+
+CREATE TABLE `appoinment` (
+  `id` int(100) NOT NULL,
+  `doctor_id` int(100) NOT NULL,
+  `doctor_name` varchar(500) NOT NULL,
+  `patient_id` int(100) NOT NULL,
+  `patient_name` varchar(500) NOT NULL,
+  `time` varchar(200) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `appoinment`
+--
+
+INSERT INTO `appoinment` (`id`, `doctor_id`, `doctor_name`, `patient_id`, `patient_name`, `time`, `date`) VALUES
+(1, 1, 'Rafiur Rashid', 1, 'Mahin', '8:00pm - 10;00 pm', '2022-05-22'),
+(2, 3, 'Rafiur Rashid', 1, 'Mahin', ' 8:00pm - 10;00 pm ', '2022-05-24'),
+(3, 3, 'Rafiur Rashid', 2, 'Naba', ' 8:00pm - 10;00 pm ', '2022-05-24');
 
 -- --------------------------------------------------------
 
@@ -89,7 +113,7 @@ CREATE TABLE `doctor` (
 INSERT INTO `doctor` (`id`, `doctor_name`, `doctor_number`, `address`, `category`, `time`, `day`, `username`, `password`, `authQues`) VALUES
 (3, 'Rafiur Rashid', '01304555701', 'Niketon', 'Eye', '8:00pm - 10;00 pm', 'Sun,Mon,Tue', 'rfr', '1234', 'blue'),
 (4, 'Salman Shamil', '01686963558', 'Rayerbag', 'Ear', '8:00pm - 10;00 pm', 'SUm,Mon,Tue', 'SHL', '1234', 'Red'),
-(5, 'A.K.M Muzahid Islam', '01686963558', 'Mirpur', 'Eye', '8:00pm - 10;00 pm', 'Sat, Wed, Tue', 'MZHD', '1234', 'Green'),
+(5, 'A.K.M Muzahid Islam', '01686963558', 'Mirpur', 'heart', '  8:00pm - 10;00 pm  ', 'Sat, Wed, Tue  ', 'MZHD  ', '1234  ', 'Green'),
 (6, 'Suman Ahmed', '01304555701', 'Rayerbag', 'Heart', '8:00pm - 10;00 pm', 'Sat, Wed, Tue', 'SMN', '123', 'Green');
 
 -- --------------------------------------------------------
@@ -116,6 +140,18 @@ INSERT INTO `faq` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `login_log`
+--
+
+CREATE TABLE `login_log` (
+  `id` int(20) NOT NULL,
+  `user_id` int(20) NOT NULL,
+  `Status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `medicine`
 --
 
@@ -131,11 +167,12 @@ CREATE TABLE `medicine` (
 --
 
 INSERT INTO `medicine` (`id`, `name`, `short_description`, `price`) VALUES
-(2, 'Zimax', 'Azithromycin is an antibiotic medication used for the treatment of a number of bacterial infections. This includes middle ear infections, strep throat, pneumonia, traveler\'s diarrhea, and certain othe', 250),
-(3, 'Pregaba', 'Azithromycin is an antibiotic useful for the treatment of bacterial infections. It is an azalide, a subclass of macrolide antibiotic. ', 100),
+(2, 'Zimax', 'Azithromycin is indicated for infections (caused by susceptible organisms) in lower respiratory tract infections including bronchitis and pneumonia, in upper respiratory tract infections including sin', 250),
+(3, 'Pregaba', 'Pregaba 75 Capsule is a medicine used to relieve pain caused by nerve damage (neuropathic pain) due to diabetes, shingles (herpes zoster infection), spinal cord injury, or other conditions. It is also', 100),
 (4, 'Monas', 'Montelukast is a selective and orally active leukotriene receptor antagonist that inhibits the cysteinyl leukotriene receptor (CysLT1). The cysteinyl leukotrienes (LTC4, LTD4, LTE4) are products of ar', 50),
 (5, 'Fexo', 'Fexo 120 mg Tablet is an effective and potent anti-allergic medicine consists of Fexofenadine. It is used to treat allergic symptoms like runny nose, watery eyes, sneezing, itching, hives, etc., assoc', 80),
-(31, 'Torax 10', 'Tablet Torax Tablet ; Indications. Ketorolac Tromethamine is indicated for the short-term management of moderate to severe acute post-operative pain.', 100);
+(31, 'Torax 10', 'Ketorolac Tromethamine is a drug of pyrrolo-pyrrole group of nonsteroidal anti-inflammatory drug (NSAID). Chemically it is known as 5 benzoyle-2,3-dihydro-1H-pyrroligine-1-carboxylic acid, compound wi', 100),
+(42, 'Osartil 25', 'Osartil 25mg Tab contains Losartan, a medicine that lowers increased blood pressure (Angiotensin-II receptor antagonists)', 80);
 
 -- --------------------------------------------------------
 
@@ -153,6 +190,13 @@ CREATE TABLE `patient_info` (
   `user_name` varchar(12) NOT NULL,
   `password` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patient_info`
+--
+
+INSERT INTO `patient_info` (`id`, `patient_name`, `patient_number`, `address`, `age`, `blood_group`, `user_name`, `password`) VALUES
+(1, '1111', '11111', '111111', 1, '11111', '1234', '1234');
 
 -- --------------------------------------------------------
 
@@ -179,6 +223,12 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `appoinment`
+--
+ALTER TABLE `appoinment`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
@@ -194,6 +244,12 @@ ALTER TABLE `doctor`
 -- Indexes for table `faq`
 --
 ALTER TABLE `faq`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_log`
+--
+ALTER TABLE `login_log`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -222,7 +278,13 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `appoinment`
+--
+ALTER TABLE `appoinment`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `booking`
@@ -234,7 +296,7 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `faq`
@@ -243,16 +305,22 @@ ALTER TABLE `faq`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `login_log`
+--
+ALTER TABLE `login_log`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `patient_info`
 --
 ALTER TABLE `patient_info`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `payment`
